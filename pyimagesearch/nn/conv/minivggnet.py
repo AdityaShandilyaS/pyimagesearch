8028d4cf28e53cd5eb7d6efe0787cbdc89638e4c
AdiShan <aditya@adappt.com>
2024-07-02 16:26:26 +0530

    captcha helper util

---
+++ pyimagesearch/nn/conv/minivggnet.py
@@ -0,0 +1,51 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers import BatchNormalization, Dropout, Dense, Flatten
+from keras.layers import Conv2D, Activation, MaxPooling2D
+from keras import backend as K
+
+
+class MiniVGGNet:
+    @staticmethod
+    def build(width, height, depth, classes):
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # first CONV => RELU => CONV => RELU => POOL layer set
+        model.add(Conv2D(32, (3, 3), padding='same', input_shape=inputShape))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(32, (3, 3), padding='same'))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # second CONV => RELU => CONV => RELU => POOL layer set
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(64, (3, 3), padding='same'))
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Dropout(0.25))
+
+        # first (and only) set of FC => RELU layers
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(BatchNormalization())
+        model.add(Dropout(0.5))
+
+        # softmax classifier
+        model.add(Dense(classes))
+        model.add(Activation('softmax'))
+
+        return model
